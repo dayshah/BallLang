2c2893b77a428f5600a61818248d1183bda74aab
dayshah <dhyeyhshah@gmail.com>
2023-10-08 00:50:15 -0400

    This is the first commit I guess

---
+++ lexer.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+enum Token {
+    tok_eof = -1,
+    tok_def = -2,
+    tok_extern = -3,
+    tok_identifier = -4,
+    tok_number = -5
+};
+
+std::string identifierStr;
+double numVal;
+
+
+
+
+
+
+
+
+int main() {
+    std::cout << "Lexing time" << "\n";
+}
