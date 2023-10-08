7383b76bd4c2b2dc8a4467bf275cc563498716a7
dayshah <dhyeyhshah@gmail.com>
2023-10-08 02:16:23 -0400

    missing rich enums and pattern matching already

--- lexer.cpp
+++ lexer.cpp
@@ -1,18 +1,74 @@
 
 #include <iostream>
+#include <unordered_map>
+#include "token.h"
 
-enum Token {
-    tok_eof = -1,
-    tok_def = -2,
-    tok_extern = -3,
-    tok_identifier = -4,
-    tok_number = -5
+struct TokenValue {
+    TokenValue(Token type, std::string value): type(type), stringVal(value) {
+        switch (type) {
+            case Token::DEF: case Token::EXTERN: case Token::IDENTIFIER:
+                break;
+            default: 
+                throw "ERROR";
+        }
+    }
+
+    TokenValue(Token type, double value): type(type), doubleVal(value) {
+        if (type != Token::NUMBER) throw "ERROR";
+    }
+
+    TokenValue(Token type): type(type) {
+        if (type != EOF) throw "ERROR";
+    }
+
+    const Token type;
+    const std::optional<std::string> stringVal;
+    const std::optional<double> doubleVal;
 };
 
-std::string identifierStr;
-double numVal;
+TokenValue getTokenValue() {
 
+    char c = getchar();
+    while (c == ' ') c = getchar();
 
+    const std::unordered_map<std::string, Token> whichToken{
+        {"def", Token::DEF},
+        {"extern", Token::EXTERN},
+    };
+
+    if (isalpha(c)) {
+        std::string identifier{};
+        do {
+            identifier.push_back(c); 
+            c = getchar();
+        } while (isalnum(c));
+        if (whichToken.find(identifier) == whichToken.end())
+            return Token::IDENTIFIER;
+        else
+            return whichToken.at(identifier);
+    }
+
+    if (isdigit(c) || c == '.') {
+        std::string number;
+        do {
+            number.push_back(c);
+            c = getchar();
+        } while (isdigit(c) || c == '.');
+        double numberVal = strtod(number.c_str(), 0);
+    }
+
+    if (c == '#') {
+        do {
+            c = getchar();
+        } while (c != EOF && c != '\n' && c != '\r');
+        if (c != EOF)
+            return getTokenValue();
+    }
+
+    if (c == EOF) return Token::ENDOFFILE;
+
+    return Token::ERROR;
+}
 
 
 
---
+++ token.h
@@ -0,0 +1,14 @@
+
+#ifndef TOKENS_INCLUDED
+#define TOKENS_INCLUDED
+
+enum Token {
+    ENDOFFILE,
+    DEF,
+    EXTERN,
+    IDENTIFIER,
+    NUMBER,
+    ERROR,
+};
+
+#endif
